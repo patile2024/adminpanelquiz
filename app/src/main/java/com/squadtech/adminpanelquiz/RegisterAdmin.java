8fe11d8e1c26d0771e8e23f395bd76f8e89d5bfb
ali-shahkust <syed.ali.shah3938@gmail.com>
2019-08-20 11:12:38 +0500

    20_aug

--- app/src/main/java/com/squadtech/adminpanelquiz/RegisterAdmin.java
+++ app/src/main/java/com/squadtech/adminpanelquiz/RegisterAdmin.java
@@ -3,9 +3,14 @@ package com.squadtech.adminpanelquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterAdmin extends AppCompatActivity {
 
+
+    private EditText eName , eEmail , ePass , ePhone ;
+    private TextView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
