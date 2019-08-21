e87c089343dee327d3ad1536339416065d1e5a61
shahrukhktk <shahrukhktk760@gmail.com>
2019-08-21 12:34:36 +0500

    Manage Quiz commit

--- app/src/main/java/com/squadtech/adminpanelquiz/CreateQuiz.java
+++ app/src/main/java/com/squadtech/adminpanelquiz/CreateQuiz.java
@@ -7,7 +7,8 @@ import android.os.Bundle;
 public class CreateQuiz extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_quiz);
     }
